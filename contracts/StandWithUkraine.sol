a50a6cb52f4c6b5c95f301f00de00c8870437c83
RubeRoyceJr <ruberoycejr@protonmail.com>
2022-02-27 12:20:22 -0500

    mintAll and price adjustment

--- contracts/StandWithUkraine.sol
+++ contracts/StandWithUkraine.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
 import './AbstractERC1155Factory.sol';
 import "@openzeppelin/contracts/utils/Strings.sol";
 
@@ -8,22 +9,18 @@ contract StandWithUkraine is AbstractERC1155Factory  {
     string ipfs;
     address multisig;
     uint256 endTime = 1648771200;
-    mapping(uint256 => uint256) public costs;
+    uint256 cost = 60000000000000000;
 
     constructor(
         string memory _name, 
         string memory _symbol,
         string memory _ipfs,
         address _multsig
-    ) ERC1155("") {
+    ) ERC1155("ipfs://") {
         name_ = _name;
         symbol_ = _symbol;
         ipfs = _ipfs;
         multisig = _multsig;
-
-        costs[0] = 10000000000000000;
-        costs[1] = 100000000000000000;
-        costs[2] = 1000000000000000000;
     }
 
     function setEndTime(uint256 _endTime) 
@@ -38,9 +35,9 @@ contract StandWithUkraine is AbstractERC1155Factory  {
         payable
     {
         require(block.timestamp <= endTime, "Mint: Not in mint window");
-        require(id <= 2, "Mint: Invalid Id");
-        require(amount <= 20, "Mint: Limit is 20");
-        require(msg.value * amount == costs[id] * amount, "Mint: Incorrect value");
+        require(id < 6, "Mint: Invalid Id");
+        require(amount <= 99, "Mint: Limit is 99");
+        require(msg.value * amount >= cost * amount, "Mint: Incorrect value");
 
         (bool sent, bytes memory data) = multisig.call{value: msg.value}("");
         require(sent, "Mint: Failed to send Ether");
@@ -48,6 +45,19 @@ contract StandWithUkraine is AbstractERC1155Factory  {
         _mint(msg.sender, id, amount, "");
     }
 
+    function mintAll(uint256 amount){
+        require(block.timestamp <= endTime, "Mint: Not in mint window");
+        require(id < 6, "Mint: Invalid Id");
+        require(amount <= 99, "Mint: Limit is 99");
+        require(msg.value * amount >= cost * amount * 6, "Mint: Incorrect value");
+
+        (bool sent, bytes memory data) = multisig.call{value: msg.value}("");
+        require(sent, "Mint: Failed to send Ether");
+
+        for (uint i = 0; i < 6; i++) {
+            _mint(msg.sender, i, amount, "");
+        }
+    }
 
     function uri(uint256 _id) public view override returns (string memory) {
             require(totalSupply(_id) > 0, "URI: nonexistent token");
